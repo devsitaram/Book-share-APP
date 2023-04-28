c0f402c0373f9ee72258c95fd52a348dbe41cb61
Sita Ram Thing <sitaramthing667@gmail.com>
2023-04-28 12:39:38 +0545

    create an logout options and change about rofile features esign

--- app/src/main/java/com/sitaram/bookshare/features/setting/SettingActivity.java
+++ app/src/main/java/com/sitaram/bookshare/features/setting/SettingActivity.java
@@ -5,9 +5,12 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.bookshare.R;
 import com.sitaram.bookshare.MainActivity;
@@ -21,23 +24,27 @@ public class SettingActivity extends AppCompatActivity {
 
     RecyclerView sRecyclerView;
     List<SettingPojo> settingPojoList;
-    Button btnBackToHome;
+    Button btnBackToHome, btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        btnBackToHome = findViewById(R.id.btnSettingToHome);
+        // call this setSettingRecyclerView methods and initialize the variable
         setSettingRecyclerView();
+        btnBackToHome = findViewById(R.id.btnSettingToHome);
+        btnLogout = findViewById(R.id.btnLogout);
 
+        // setOnClickListener methods in btnBackToHome button
         btnBackToHome.setOnClickListener(v -> navigateHomePage());
-
+        btnLogout.setOnClickListener(v -> setLogoutAlertDialog());
     }
 
     // navigate setting to home page
-    private void navigateHomePage(){
-        Intent intent = new Intent(SettingActivity.this, MainActivity.class);
-        startActivity(intent);
+    private void navigateHomePage() {
+        startActivity(new Intent(SettingActivity.this, MainActivity.class));
     }
+
     public void setSettingRecyclerView() {
         sRecyclerView = findViewById(R.id.rvUserSetting);
         settingPojoList = new ArrayList<>();
@@ -53,7 +60,23 @@ public class SettingActivity extends AppCompatActivity {
         sRecyclerView.setLayoutManager(linearLayoutManager);
 
         // add divider
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(sRecyclerView.getContext(),linearLayoutManager.getOrientation());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(sRecyclerView.getContext(), linearLayoutManager.getOrientation());
         sRecyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    private void setLogoutAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
+        builder.setTitle("Log out?");
+        builder.setMessage("Are you sure you want to log out?");
+        // press the yes the logout the app
+        builder.setPositiveButton("Yes", (DialogInterface dialog, int i) -> {
+            Toast.makeText(this, "Thank you", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+        // press the No then cancel to logout the app
+        builder.setNegativeButton("No", (DialogInterface dialogInterface, int i) -> {
+            dialogInterface.dismiss();
+        });
+        builder.show();
     }
 }
